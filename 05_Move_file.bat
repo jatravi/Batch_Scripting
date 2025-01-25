b287cfdec971be6c1360cf211448f3766f7ac7f5
Ravi Choudhary <151042374+jatravi@users.noreply.github.com>
2025-01-25 14:03:24 +0530

    Create Move.bat

---
+++ 05_Move_file.bat
@@ -0,0 +1,6 @@
+@echo off
+Tilte Move_file
+echo Moving file from one place to another
+pause
+move "C:\Users\Monu Jat\OneDrive\Desktop\CODING\FULL STACK WEB DEV\*.html" "C:\Users\Monu Jat\OneDrive\Desktop\CODING\FULL STACK WEB DEV\CSS"
+exit
